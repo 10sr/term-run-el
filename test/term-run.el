29892606579212e4251b8744cdc171ff0a0fefd2
10sr <8slashes+git@gmail.com>
2015-02-23 13:57:59 +0900

    Fix variable names

--- test/term-run.el
+++ test/term-run.el
@@ -1,21 +1,21 @@
 (require 'term-run)
 
-(defmacro ert-term-run-make-output (cmd str)
-  "Judge if CMD will generate STR as output."
+(defmacro ert-term-run-make-output (cmds regexp)
+  "Judge if CMDS will generate REGEXP as output."
   `(with-current-buffer (apply 'term-run
-                               (car ,cmd)
+                               (car ,cmds)
                                nil
-                               (cdr ,cmd))
+                               (cdr ,cmds))
      (sleep-for 0.5)
      (goto-char (point-min))
-     (search-forward-regexp ,str)))
+     (search-forward-regexp ,regexp)))
 
-(defmacro ert-term-run-shell-command-make-output (cmd str)
-  "Judge if CMD will generate STR as output."
-  `(with-current-buffer (term-run-shell-command ,cmd)
+(defmacro ert-term-run-shell-command-make-output (commandline regexp)
+  "Judge if COMMANDLINE will generate REGEXP as output."
+  `(with-current-buffer (term-run-shell-command ,commandline)
      (sleep-for 0.5)
      (goto-char (point-min))
-     (search-forward-regexp ,str)))
+     (search-forward-regexp ,regexp)))
 
 (ert-deftest test-term-run ()
   (ert-term-run-make-output '("printf" "%04d" "112")
