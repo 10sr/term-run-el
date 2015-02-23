7f73dad692ed19d78ce8b7dd0965de9f30352982
10sr <8slashes+git@gmail.com>
2015-02-23 13:48:14 +0900

    Use regexp for testing

--- test/term-run.el
+++ test/term-run.el
@@ -8,19 +8,19 @@
                                (cdr ,cmd))
      (sleep-for 0.5)
      (goto-char (point-min))
-     (search-forward ,str)))
+     (search-forward-regexp ,str)))
 
 (defmacro ert-term-run-shell-command-make-output (cmd str)
   "Judge if CMD will generate STR as output."
   `(with-current-buffer (term-run-shell-command ,cmd)
      (sleep-for 0.5)
      (goto-char (point-min))
-     (search-forward ,str)))
+     (search-forward-regexp ,str)))
 
 (ert-deftest test-term-run ()
   (ert-term-run-make-output '("printf" "%04d" "112")
-                            "0112"))
+                            "^0112$"))
 
 (ert-deftest test-term-run-shell-command ()
   (ert-term-run-shell-command-make-output "printf '%05d' 345"
-                                          "00345"))
+                                          "^00345$"))
